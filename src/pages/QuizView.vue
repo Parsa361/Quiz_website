9767e3961ce1ae36e641f72f2ec3d7503142e243
Parsa Farahani <parsafarahany821@gmail.com>
2023-04-06 22:50:21 +0330

    Sends props and receive emit from it's child

--- src/pages/QuizView.vue
+++ src/pages/QuizView.vue
@@ -1,32 +1,42 @@
 <script setup>
 import Question from '../components/Question.vue';
 import QuizHeader from '../components/QuizHeader.vue'
+import Result from '../components/Result.vue'
 import { useRoute } from 'vue-router';
 import quizes from '../data/quizes.json'
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import { computed } from '@vue/reactivity';
 
 const route = useRoute()
-
 const quizId = parseInt(route.params.id)
-
 const quiz = quizes.find(q => q.id === quizId)
-
 const currentQuestionIndex = ref(0)
+const numberOfCorrectAnswers = ref(0)
+const showResults = ref(false)
 
-const questionStatus = computed(() => {
-    return `${currentQuestionIndex.value}/${quiz.questions.length}`
-})
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+const barPercentage = computed(() => `${currentQuestionIndex.value / quiz.questions.length * 100}%`)
+
+const onOptionSelected = (isCorrect) => {
+    if (isCorrect && numberOfCorrectAnswers.value < 3) {
+        numberOfCorrectAnswers.value++
+    }
+    if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+        showResults.value = true
+    }
+    currentQuestionIndex.value++
+}
 </script>
 
 <template>
     <div>
-        <QuizHeader :questionStatus="questionStatus" />
+        <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
         <div>
-            <Question :question="quiz.questions[currentQuestionIndex]" />
+            <Question v-if="!showResults" @selectOption="onOptionSelected"
+                :question="quiz.questions[currentQuestionIndex]" />
+            <Result v-else :quizQuestionLength="quiz.questions.length" :numberOfCorrectAnswers="numberOfCorrectAnswers" />
         </div>
     </div>
-    <button @click="currentQuestionIndex++">Next Q</button>
 </template>
 
 
